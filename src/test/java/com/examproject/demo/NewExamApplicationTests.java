148d268ca6ba029c9be2955647cc2c9bb6167b0e
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-17 09:33:55 +0100

    Initial commit

---
+++ src/test/java/com/examproject/demo/NewExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
